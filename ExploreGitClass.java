68bc6c6851bb8f37a7b87a251fbe446673790ff5
PriyankaSharma-Git <priyankasharma.dli@gmail.com>
2016-07-18 16:07:52 +0530

    added file via upload file method

---
+++ ExploreGitClass.java
@@ -0,0 +1,11 @@
+package exploreGitPack;
+
+public class ExploreGitClass {
+	public static void main(String args[])
+	{
+		System.out.println("PS-mygit repo,egit exp.");
+		System.out.println(" PS-sharma");
+		//my git repo clone url: https://github.com/PriyankaSharma-Git/GitExplore.git
+	}
+
+}
